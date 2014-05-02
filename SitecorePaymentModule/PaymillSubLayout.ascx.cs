00cd7793a238a01b4f2fcef30d83dd7a246c303f
vladimir <vladimir.marinov@bulgariaspect.net>
2014-05-02 19:08:34 +0300

    Sitecore module v.1.0

---
+++ SitecorePaymentModule/PaymillSubLayout.ascx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PaymillWrapper;
+using PaymillWrapper.Models;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace Paymill.Sitecore.Sublayouts
+{
+    /// <summary>
+    /// User control( sublayout) that make Payment Transaction. 
+    /// </summary>
+    public partial class PaymentSublayout : System.Web.UI.UserControl
+    {
+        /// <summary>
+        /// Delegate for parent control to receive response from Paymill . 
+        /// </summary>
+        public delegate void TransactionSubmitedDelegate(Transaction trans);
+
+        /// <summary>
+        /// Set this public property to receive response from Paymill. 
+        /// </summary>
+        public TransactionSubmitedDelegate TransactionSubmited { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+        /// <summary>
+        /// On Submit button click. 
+        /// </summary>
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            PaymillContext paymill = new PaymillContext("YOUR PRIVATE KEY");
+            String token = hToken.Value;
+            Payment payment = paymill.PaymentService.CreateWithTokenAsync(token).Result;
+            int amount = int.Parse(tbAmount.Text);
+            Transaction transaction = paymill.TransactionService.CreateWithPaymentAsync(payment, amount, tbCurrency.Text, "Test API c#").Result;
+            /// Yout Transaction Is Complete 
+            if (TransactionSubmited != null)
+            {
+                TransactionSubmited(transaction);
+            }
+        }
+    }
+}
---
+++ SitecorePaymentModule/README.md
@@ -0,0 +1,28 @@
+![PAYMILL icon](https://static.paymill.com/r/335f99eb3914d517bf392beb1adaf7cccef786b6/img/logo-download_Light.png)
+
+# PAYMILL .NET
+
+A .NET wrapper for the  [PAYMILL](https://www.paymill.com/) API.
+
+## Getting started
+
+
+## Installation Module
+
+Please install Paymill Wrapper for the PAYMILL API, run the following command in the [Package Manager Console](http://docs.nuget.org/docs/start-here/using-the-package-manager-console)
+
+
+### Using 
+
+```cs
+    <link rel="stylesheet" type="text/css" href="css/paymill_styles.css" /> 
+    <script src="js/jquery.min.js"></script>%
+    <script type="text/javascript" src="https://bridge.paymill.de/"></script>
+    <script type="text/javascript" src="js/creditcard_brand_detection.js"></script>
+```
+
+## License
+
+Copyright 2014 PAYMILL GmbH.
+
+MIT License (enclosed)
