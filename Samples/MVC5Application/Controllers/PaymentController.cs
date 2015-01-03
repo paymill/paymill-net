71fe4f4d41a0c35c0f4a0f22f18e69ee016949dd
vladimir <vladimir.marinov@bulgariaspect.net>
2015-01-04 00:02:21 +0200

    moved samples

---
+++ Samples/MVC5Application/Controllers/PaymentController.cs
@@ -0,0 +1,53 @@
+﻿using PaymillWrapper;
+using PaymillWrapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5Application.Controllers
+{
+    public class PaymentController : Controller
+    {
+        //
+        // GET: /Default/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public ActionResult Submit(Models.PaymillForm form)
+        {
+            PaymillContext paymill = new PaymillContext("YOUR Private Key");
+            string token = Request.Form["hToken"];
+            try
+            {
+                Transaction transaction = paymill.TransactionService.CreateWithTokenAsync(token, form.Amount, form.Currency).Result;
+                Transaction.TransactionStatus status = transaction.Status;
+                // You can check the transaction status like this : 
+               /* if (status == Transaction.TransactionStatus.PENDING)
+                {
+                    return Content("Payment PENDING");
+                }*/
+
+                // You can check the Response Code like this : 
+                if (transaction.ResponseCode == 2000)
+                {
+                    return Content("Payment Success");
+                }
+                else
+                {
+                    return Content("Payment NOT successful, response code " + transaction.ResponseCode);
+                }
+            }
+            catch(AggregateException  ex){
+                // or returns Error from server
+                return Content(ex.InnerException.Message);
+            }
+            return Content(""); 
+        } 
+    }
+}
---
+++ Samples/MVC5Application/Models/PaymillForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc5Application.Models
+{
+    public class PaymillForm
+    {
+        public String CardNumber { get; set; }
+        public String CardExpiry { get; set; }
+        public String CardHoldername { get; set; }
+        public String CardCvc { get; set; }
+        public int Amount { get; set; }
+        public String Currency { get; set; }
+    }
+}
